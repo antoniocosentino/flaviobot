0bf353e9301e6d9782219616d3758e3ef1d2ac5b
Antonio Cosentino <hello@antoniocosentino.it>
2022-11-13 21:24:55 +0100

    added some more tests

--- __tests__/utilities.test.ts
+++ __tests__/utilities.test.ts
@@ -23,10 +23,52 @@ describe('getWinners', () => {
 
         expect(response).toEqual(['U5ZDPV5S6', 'U5QJXTGR1']);
     });
+
+    it('should get the correct list of winners - 3 participants / 3 winners', async () => {
+        const correctWord = 'BOSCO';
+
+        const participantsWords = {
+            U5ZDPV5S6: {
+                word: 'BOSCO',
+                sentAt: 1667944391,
+            },
+            U5QJXTGR1: {
+                word: 'BOSCO',
+                sentAt: 1667944392,
+            },
+            U5X56H0TG: {
+                word: 'BOSCO',
+                sentAt: 1667944393,
+            },
+        };
+
+        const response = getWinners(correctWord, participantsWords);
+
+        expect(response).toEqual(['U5ZDPV5S6', 'U5QJXTGR1', 'U5X56H0TG']);
+    });
+
+    it('should get the correct list of winners - 2 participants / no winners', async () => {
+        const correctWord = 'BOSCO';
+
+        const participantsWords = {
+            U5ZDPV5S6: {
+                word: 'ALBERO',
+                sentAt: 1667944391,
+            },
+            U5QJXTGR1: {
+                word: 'ALBERO',
+                sentAt: 1667944392,
+            },
+        };
+
+        const response = getWinners(correctWord, participantsWords);
+
+        expect(response).toEqual([]);
+    });
 });
 
 describe('getUpdatedScores', () => {
-    it('should update the scores correctly', async () => {
+    it('should update the scores correctly - 3 participants / 2 winners', async () => {
         const winners = ['U5ZDPV5S6', 'U5QJXTGR1'];
 
         const participantsWords = {
@@ -75,6 +117,128 @@ describe('getUpdatedScores', () => {
             {
                 user: 'U5QJXTGR1',
                 score: 4,
+            },
+            {
+                user: 'U5X56H0TG',
+                score: 3,
+            },
+            {
+                user: 'U5Q1D5LE4',
+                score: 0,
+            },
+        ]);
+    });
+
+    it('should update the scores correctly - 3 participants / 3 winners', async () => {
+        const winners = ['U5ZDPV5S6', 'U5QJXTGR1', 'U5X56H0TG'];
+
+        const participantsWords = {
+            U5ZDPV5S6: {
+                word: 'BOSCO',
+                sentAt: 1667944392,
+            },
+            U5QJXTGR1: {
+                word: 'BOSCO',
+                sentAt: 1667944391,
+            },
+            U5X56H0TG: {
+                word: 'BOSCO',
+                sentAt: 1667944393,
+            },
+        };
+
+        const sessionScores = {
+            results: [
+                {
+                    user: 'U5ZDPV5S6',
+                    score: 1,
+                },
+                {
+                    user: 'U5QJXTGR1',
+                    score: 2,
+                },
+                {
+                    user: 'U5X56H0TG',
+                    score: 3,
+                },
+                {
+                    user: 'U5Q1D5LE4',
+                    score: 0,
+                },
+            ],
+        };
+
+        const response = getUpdatedScores(participantsWords, winners, sessionScores);
+
+        expect(response).toEqual([
+            {
+                user: 'U5ZDPV5S6',
+                score: 2,
+            },
+            {
+                user: 'U5QJXTGR1',
+                score: 3,
+            },
+            {
+                user: 'U5X56H0TG',
+                score: 4,
+            },
+            {
+                user: 'U5Q1D5LE4',
+                score: 0,
+            },
+        ]);
+    });
+
+    it('should update the scores correctly - 3 participants / no winners', async () => {
+        const winners = [];
+
+        const participantsWords = {
+            U5ZDPV5S6: {
+                word: 'BOSCO',
+                sentAt: 1667944392,
+            },
+            U5QJXTGR1: {
+                word: 'BOSCO',
+                sentAt: 1667944391,
+            },
+            U5X56H0TG: {
+                word: 'BOSCO',
+                sentAt: 1667944393,
+            },
+        };
+
+        const sessionScores = {
+            results: [
+                {
+                    user: 'U5ZDPV5S6',
+                    score: 1,
+                },
+                {
+                    user: 'U5QJXTGR1',
+                    score: 2,
+                },
+                {
+                    user: 'U5X56H0TG',
+                    score: 3,
+                },
+                {
+                    user: 'U5Q1D5LE4',
+                    score: 0,
+                },
+            ],
+        };
+
+        const response = getUpdatedScores(participantsWords, winners, sessionScores);
+
+        expect(response).toEqual([
+            {
+                user: 'U5ZDPV5S6',
+                score: 1,
+            },
+            {
+                user: 'U5QJXTGR1',
+                score: 2,
             },
             {
                 user: 'U5X56H0TG',
